21c30ec582c612454eb404fb6fbbcd60bf16e844
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-03-29 23:57:10 +0700

    เพิ่มตาราง car ที่หน้า admin

--- ChaoCarHub/src/components/Admin/Table_Car.vue
+++ ChaoCarHub/src/components/Admin/Table_Car.vue
@@ -6,7 +6,32 @@ onMounted(CarStore.FetchCar);
 </script>
 
 <template>
-  <div v-for="item in CarStore.carvalue">
-        {{ item.car_id }}
-  </div>
+  <table style="width: 100%">
+    <tr class="has-text-centered">
+      <th>รหัสรถ</th>
+      <th>รูปรถ</th>
+      <th>ชื่อรถ</th>
+      <th>ยี่ห้อรถ</th>
+      <th>รุ่นรถ</th>
+      <th>จำนวนที่นั่ง</th>
+      <th>จำนวนใส่กระเป๋า</th>
+      <th>ราคา</th>
+    </tr>
+    <tr v-for="item in CarStore.carvalue">
+      <td>{{ item.car_id }}</td>
+      <td>
+        <div class="card-image px-5 pt-2">
+          <figure class="image is-4by3">
+            <img :src="item.car_img" alt="Placeholder image " />
+          </figure>
+        </div>
+      </td>
+      <td>{{ item.car_name }}</td>
+      <td>{{ item.car_brand }}</td>
+      <td>{{ item.car_model }}</td>
+      <td>{{ item.car_seat }}</td>
+      <td>{{ item.car_bag }}</td>
+      <td>{{ item.car_rentprice }}</td>
+    </tr>
+  </table>
 </template>
--- ChaoCarHub/src/views/Admin.vue
+++ ChaoCarHub/src/views/Admin.vue
@@ -1,10 +1,11 @@
 <script setup>
 import HeroUser from "../components/HeroUser.vue";
+import Table_Car from "../components/Admin/Table_Car.vue";
 </script>
 
 <template>
 <HeroUser></HeroUser>
-
+<Table_Car></Table_Car>
 </template>
 
 
