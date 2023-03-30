74b5a0f7bae792ec68311b95307ea9ce6dae8098
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-03-30 23:00:24 +0700

    Update index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -9,9 +9,7 @@ router.post('/car', async function(req, res, next){
     try{
         const [rows, fields] = await pool.query("INSERT INTO car (`car_id`, `car_img`, `car_name`, `car_brand`, `car_model`, `car_seat`, `car_bag`, `car_rentprice`) value(?, ?, ?, ?, ?, ?, ?, ?)", 
         [car_id, car_img, car_name, car_brand, car_model, car_seat, car_bag, car_rentprice]);
-        // return json ของรายการ blogs
         return res.json({"message":`A new car is added`});
-        // return res.json({"message":`A new comment is added (ID: ${rows.insertID})`});
       } catch (err) {
         console.log(err)
         return next(err);
