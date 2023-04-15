9c2942e836939f0f5d4c91afbafbd086d411adf9
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-04-15 16:05:13 +0700

    แก้ไข ui

---
+++ ChaoCarHub/src/components/HeroAdmin.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  created() {
+    this.showname = JSON.parse(localStorage.getItem("myname"));
+    console.log(this.showname);
+  },
+};
+</script>
+
+<template>
+  <section class="hero is-success is-halfheight is-gradient column">
+    <div class="hero-body columns">
+      <div class="column is-4">
+        <p class="title">
+          {{ showname }}
+        </p>
+        <p class="subtitle">Track your rental status here!</p>
+      </div>
+      <div class="column">
+        <div class="columns">
+            <div class="column is-4 has-text-centered">
+                <router-link to="/tablecar">
+                   <div class="card p-4">
+                    <div class="card-image">
+                            <img src="https://cdn-icons-png.flaticon.com/512/871/871867.png" alt="Placeholder image" width="150" height="150">
+                    </div>
+                    <b>CRUD รถในระบบ</b>
+                </div> 
+                </router-link>
+            </div>
+            <div class="column is-4 has-text-centered">
+                <router-link to="/tablecar">
+                   <div class="card p-4">
+                    <div class="card-image">
+                            <img src="https://cdn-icons-png.flaticon.com/512/4906/4906266.png" alt="Placeholder image" width="150" height="150">
+                    </div>
+                    <b>รายละเอียดจองรถของลูกค้า</b>
+                </div> 
+                </router-link>
+            </div>
+            <div class="column is-4 has-text-centered">
+                <router-link to="/tablecar">
+                   <div class="card p-4">
+                    <div class="card-image">
+                            <img src="https://cdn-icons-png.flaticon.com/512/4422/4422509.png" alt="Placeholder image" width="150" height="150">
+                    </div>
+                    <b>ตรวจสอบการคืนรถและซ่อมบำรุง</b>
+                </div> 
+                </router-link>
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- ChaoCarHub/src/views/Admin.vue
+++ ChaoCarHub/src/views/Admin.vue
@@ -1,12 +1,10 @@
 <script setup>
-import HeroUser from "../components/HeroUser.vue";
+import HeroAdmin from "../components/HeroAdmin.vue";
 import Table_Car from "../components/Admin/Table_Car.vue";
 </script>
 
 <template>
-<HeroUser></HeroUser>
-<router-link to="/tablecar"><div class="button has-background-primary is-size-5 has-text-white">ตารางสำหรับเพิ่ม/ลบ/แก้ไข/แสดงรายละเอียดข้อมูลรถ</div></router-link>
-
+<HeroAdmin/>
 <!-- <Table_Car></Table_Car> -->
 </template>
 
