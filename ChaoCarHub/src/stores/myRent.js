a7ef84b094c1185a302207d9bd6a9aac58822bbe
CHOMPOO (ชมพู่) <88492141+DuangkamonPhobsungnoen@users.noreply.github.com>
2023-05-16 12:26:22 +0700

    ปุ่ม delete

--- ChaoCarHub/src/stores/myRent.js
+++ ChaoCarHub/src/stores/myRent.js
@@ -26,24 +26,42 @@ export const UsemyrentStore = defineStore('myrent', () => {
     historyCar.value = mycar.value.filter((car) => car.r_status === 'history')
   }
 
+  const showAlert = ref(false);
+  const alertMessage = ref('');
+  const confirmResult = ref(null);
 
-  async function cancel(rentId) {
-    console.log(rentId)
-    const fetchingData = await axios.post("/myrent/remove", {
-      rentId: parseInt(rentId)
-    })
-  }
+    async function showConfirmation(carBrand, carModel) {
+      showAlert.value = true;
+      alertMessage.value = `คุณต้องการยกเลิกการจองรถ ${carBrand} ${carModel}` ;
+    };
 
+    async function confirm(result, rentId) {
+      confirmResult.value = result;
+      showAlert.value = false;
 
+      if (result) {
+        // ถ้ากดตกลงก็จะลบ card
+        const fetchingData = await axios.post("/myrent/remove", {
+        rentId: parseInt(rentId) 
+        })
+        
+        checkoutCar.value = checkoutCar.value.filter((car) => car.r_id !== rentId)
+      } 
+      console.log(rentId)
+    }
 
   return {
     myrentCar,
     mycar,
-    cancel,
     mycar,
     checkoutCar,
     pickupCar,
     returnCar,
-    historyCar
+    historyCar,
+    showAlert,
+    alertMessage,
+    confirmResult,
+    showConfirmation,
+    confirm
   }
 })
